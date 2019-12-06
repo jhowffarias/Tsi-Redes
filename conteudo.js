2777dbb5197a3ea3940d18ea17341f8127be7564
jhowffarias <49926322+jhowffarias@users.noreply.github.com>
2019-12-06 19:53:11 -0300

    .

---
+++ conteudo.js
@@ -0,0 +1,67 @@
+document.getElementById("HTTP").onclick = function () {
+
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            document.querySelector("#texto").innerHTML = ajax.responseText;
+            document.getElementById("conthttp").style.display = "block";
+            document.getElementById("requisicao").style.display = "none";
+            document.getElementById("funcionamento").style.display = "none";
+            document.getElementById("https").style.display = "none";
+        }
+    }
+    ajax.open("GET", "http.php")
+    ajax.send();
+
+}
+
+document.getElementById("requisicao").onclick = function () {
+
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            document.querySelector("#texto").innerHTML = ajax.responseText;
+            document.getElementById("conthttp").style.display = "none";
+            document.getElementById("requisicao").style.display = "block";
+            document.getElementById("funcionamento").style.display = "none";
+            document.getElementById("https").style.display = "none";
+        }
+    }
+    ajax.open("GET", "requisicao.php")
+    ajax.send();
+
+}
+
+document.getElementById("funcionamento").onclick = function () {
+
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            document.querySelector("#texto").innerHTML = ajax.responseText;
+            document.getElementById("conthttp").style.display = "none";
+            document.getElementById("requisicao").style.display = "none";
+            document.getElementById("funcionamento").style.display = "block";
+            document.getElementById("https").style.display = "none";
+        }
+    }
+    ajax.open("GET", "funcionamento.php")
+    ajax.send();
+
+}
+
+document.getElementById("https").onclick = function () {
+
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            document.querySelector("#texto").innerHTML = ajax.responseText;
+            document.getElementById("conthttp").style.display = "none";
+            document.getElementById("requisicao").style.display = "none";
+            document.getElementById("funcionamento").style.display = "none";
+            document.getElementById("https").style.display = "block";
+        }
+    }
+    ajax.open("GET", "https.php")
+    ajax.send();
+
+}
